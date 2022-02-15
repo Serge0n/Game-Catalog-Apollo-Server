d83ccc74f366cb3a31474379a92c730c265a4bee
Sergey Kravchyonok <siarhei.krauchonak@artsymail.com>
2022-02-15 17:40:59 +0300

    User updates

--- src/resolvers/userResolver.ts
+++ src/resolvers/userResolver.ts
@@ -1,13 +1,25 @@
+import * as bcrypt from "bcryptjs"
+import { Context } from "context"
+import * as jwt from "jsonwebtoken"
+import { APP_SECRET } from "../utils/auth"
+
 export const userResolver = {
   Query: {
-    user: async (parent: any, args: any, context: { user: any }) => {
-      if (!context.user) return null;
-      return context.user
+    user: async (parent: any, args: any, context: Context) => {
+      if (!context.userId) return null
+      return context.prisma.user.findUnique({ where: { id: context.userId } })
     }
   },
-  // Mutation: {
-  //   signup: async (parent: any, args: any, context: any) => {
-
-  //   }
-  // }
+  Mutation: {
+    signup: async (parent: any, args: any, context: any) => {
+      const { email, name } = args;
+      const password = await bcrypt.hash(args.password, 10)
+      const user = await context.prisma.user.create({
+        data: { email, name, password },
+    });
+      const token = jwt.sign({ userId: user.id }, APP_SECRET)
+      
+      return { token, user }
+    }
+  }
 }
